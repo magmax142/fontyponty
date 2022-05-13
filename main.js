15bb45c4eeba71a8e8c60250a8ec33c264ebba8f
magmax142 <78598575+magmax142@users.noreply.github.com>
2022-05-13 11:22:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+function setup(){
+    canvas=createCanvas(550, 550);
+    canvas.position(1000, 200);
+    screen_width = window.innerWidth;
+    screen_height=window.innerHeight;
+}
+canvas=createCanvas(){
+    screen_width=150;
+    screen_height=150;
+}
